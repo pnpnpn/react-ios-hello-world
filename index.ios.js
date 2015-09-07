61e12d6aea390a33c83b6dbc967b7be047963eac
Patrick <pn.appdev@gmail.com>
2015-09-07 18:52:55 -0400

    working sqlite DB

--- index.ios.js
+++ index.ios.js
@@ -22,11 +22,38 @@ class PropertyFinderApp extends React.Component {
   }
 }
 
-class HelloWorld extends React.Component {
-  render() {
-    return <React.Text style={styles.text}>Hello World (Again)</React.Text>;
+//class HelloWorld extends React.Component {
+//  render() {
+//    return <React.Text style={styles.text}>Hello World (Again)</React.Text>;
+//  }
+//}
+
+var sqlite = require('react-native-sqlite');
+sqlite.open("my_first_db.sqlite", function (error, database) {
+  if (error) {
+    console.log("Failed to open database:", error);
+    return;
   }
-}
+  var sql = "SELECT first, last FROM t1";
+  //var params = ["Smith"];
+  database.executeSQL(sql, [], rowCallback, completeCallback);
+  function rowCallback(rowData) {
+    console.log("Got row data:", rowData);
+  }
+  function completeCallback(error) {
+    if (error) {
+      console.log("Failed to execute query:", error);
+      return
+    }
+    console.log("Query complete!");
+    database.close(function (error) {
+      if (error) {
+        console.log("Failed to close database:", error);
+        return
+      }
+    });
+  }
+})
 
 React.AppRegistry.registerComponent('PropertyFinder',
                                     function() { return PropertyFinderApp });
